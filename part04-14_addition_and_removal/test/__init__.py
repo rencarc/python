bfcd5e1e70775408923df82a671c11f4831c815c
zhen <180519774+rencarc@users.noreply.github.com>
2025-04-07 07:54:21 +0200

    Add files via upload

---
+++ part04-14_addition_and_removal/test/__init__.py
@@ -0,0 +1,21 @@
+my_list = []
+next_item = 1
+
+while True:
+    print(f"The list is now {my_list}")
+    choice = input("a(d)d, (r)emove or e(x)it: ")
+    if choice == "d": 
+        my_list.append(next_item)
+        next_item += 1
+    elif choice == "r":
+        if my_list != []:
+            my_list.pop()
+    elif choice == "x":
+        print("Bye!")
+        break
+    
+
+
+
+
+
