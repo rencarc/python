6fd714303d9733f42f1a1f07ec6996611e7cb0df
zhen <180519774+rencarc@users.noreply.github.com>
2025-04-26 07:36:26 +0200

    Add files via upload

---
+++ part05-09_remove_smallest/src/remove_smallest.py
@@ -0,0 +1,15 @@
+# Write your solution here
+# Write your solution here
+def remove_smallest(numbers: list):
+
+    if numbers:
+        smallest = min(numbers)
+        numbers.remove(smallest)
+
+
+  
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 1, 3, 5]
+    remove_smallest(numbers)
+    print(numbers)
