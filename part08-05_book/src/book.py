ab59b007c6dce597f50f463bb2c2d0405df83d89
zhen <180519774+rencarc@users.noreply.github.com>
2025-07-12 07:50:27 +0200

    Add files via upload

---
+++ part08-05_book/src/book.py
@@ -0,0 +1,9 @@
+
+class Book:
+
+    def __init__(self, name, author, genre, year):
+        self.name = name
+        self.author = author 
+        self.genre = genre 
+        self.year = year 
+        
